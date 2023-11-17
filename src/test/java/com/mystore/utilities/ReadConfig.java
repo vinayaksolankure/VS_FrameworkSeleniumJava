bbcb07b11d5b958ffeda10f9e9fa6aee28c3d20b
HP <HP@DESKTOP-9RG7NDH>
2023-11-17 09:16:21 +0530

    Updated path for config file in readconfig

--- src/test/java/com/mystore/utilities/ReadConfig.java
+++ src/test/java/com/mystore/utilities/ReadConfig.java
@@ -9,7 +9,7 @@ public class ReadConfig {
 	Properties properties;
 
 	// Below path is config.properties file path.
-	String path = "D:\\Eclipse Project\\Github Projects\\VS_FrameworkSeleniumJava\\Configuration\\config.properties";
+	String path = ".//Configuration/config.properties";
 
 	public ReadConfig() {
 		try {
